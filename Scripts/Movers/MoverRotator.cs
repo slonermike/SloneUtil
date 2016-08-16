86fd9ace97465a3d2533572af1ac3f0f3c4c0131
slonermike <michaelgareth@gmail.com>
2016-08-15 18:29:23 -0700

    Added MoverRotator mover script to SloneUtil

---
+++ Scripts/Movers/MoverRotator.cs
@@ -0,0 +1,37 @@
+﻿/************************************************************
+ * 
+ *                     Music Player
+ *                 2016 Slonersoft Games
+ * 
+ * Continually rotates around each of the 3 axes according to
+ * the rotateVector.
+ * 
+ ************************************************************/
+
+using UnityEngine;
+using System.Collections;
+
+public class MoverRotator : MonoBehaviour {
+
+	[Tooltip("Each axis can have a different amount of rotation.")]
+	public Vector3 rotateVector;
+
+	[Tooltip("True to rotate locally, false to rotate globally.")]
+	public bool localRotation = false;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (localRotation) {
+			transform.Rotate (rotateVector * Time.deltaTime);
+		} else {
+			transform.RotateAround (transform.position, Vector3.right, rotateVector.x * Time.deltaTime);
+			transform.RotateAround (transform.position, Vector3.up, rotateVector.y * Time.deltaTime);
+			transform.RotateAround (transform.position, Vector3.forward, rotateVector.z * Time.deltaTime);
+		}
+	}
+}
