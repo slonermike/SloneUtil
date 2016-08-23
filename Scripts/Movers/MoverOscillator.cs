0a648e2689d5f385e04b31354b3938c1e154b727
slonermike <michaelgareth@gmail.com>
2016-08-23 09:21:47 -0700

    Added mover scripts for oscillation along and around 3 axes.

---
+++ Scripts/Movers/MoverOscillator.cs
@@ -0,0 +1,53 @@
+﻿/************************************************************
+ * 
+ *                    Mover Oscillator
+ *                 2016 Slonersoft Games
+ * 
+ * Continually oscillates along each of the 3 axes according to
+ * the specified magnitude and frequency.
+ * 
+ ************************************************************/
+
+using UnityEngine;
+using System.Collections;
+
+public class MoverOscillator : MonoBehaviour {
+
+	[Tooltip("Magnitude of oscillation along each axis.")]
+	public Vector3 magnitude;
+
+	[Tooltip("Frequency of oscillation along each axis.")]
+	public Vector3 frequency;
+
+	[Tooltip("Time offset for starting oscillation.")]
+	public Vector3 startOffset;
+
+	[Tooltip("True to oscillate in local space, false for global space.")]
+	public bool localMotion = true;
+
+	private float startTime;
+	private Vector3 startPos;
+
+	// Use this for initialization
+	void Start () {
+		startTime = Time.time;
+		startPos = localMotion ? transform.localPosition : transform.position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		float time = startTime - Time.time;
+
+		float xOffset = magnitude.x * Mathf.Sin ((time + startOffset.x) * frequency.x);
+		float yOffset = magnitude.y * Mathf.Sin ((time + startOffset.y) * frequency.y);
+		float zOffset = magnitude.z * Mathf.Sin ((time + startOffset.z) * frequency.z);
+		Vector3 offset = new Vector3 (xOffset, yOffset, zOffset);
+
+		if (localMotion) {
+			transform.localPosition = startPos + offset;
+		} else {
+			transform.position = startPos + offset;
+		}
+	}
+}
---
+++ Scripts/Movers/MoverRotateOscillator.cs
@@ -0,0 +1,46 @@
+﻿/************************************************************
+ * 
+ *                Mover Rotate Oscillator
+ *                 2016 Slonersoft Games
+ * 
+ * Continually oscillates around each of the 3 axes according to
+ * the specified magnitude and frequency.
+ * 
+ ************************************************************/
+
+using UnityEngine;
+using System.Collections;
+
+public class MoverRotateOscillator : MonoBehaviour {
+
+	[Tooltip("Magnitude of oscillation around each axis.")]
+	public Vector3 magnitude;
+
+	[Tooltip("Frequency of oscillation around each axis.")]
+	public Vector3 frequency;
+
+	[Tooltip("Time offset for starting each axis of oscillation.")]
+	public Vector3 startOffset;
+
+	Vector3 startRotation;
+	float startTime;
+
+	// Use this for initialization
+	void Start () {
+		startTime = Time.time;
+		startRotation = transform.localRotation.eulerAngles;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float time = startTime - Time.time;
+
+		float xOffset = magnitude.x * Mathf.Sin ((time + startOffset.x) * frequency.x);
+		float yOffset = magnitude.y * Mathf.Sin ((time + startOffset.y) * frequency.y);
+		float zOffset = magnitude.z * Mathf.Sin ((time + startOffset.z) * frequency.z);
+
+		Vector3 offset = new Vector3 (xOffset, yOffset, zOffset);
+
+		transform.localEulerAngles = startRotation + offset;
+	}
+}
